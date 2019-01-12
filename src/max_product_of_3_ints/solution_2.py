df95afaec6f0520efed082a9c7818fc4adb6ba7d
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-01-12 11:27:38 +0000

    Max prod of 3 ints - both +ve & -ve numbers

--- src/max_product_of_3_ints/solution_2.py
+++ src/max_product_of_3_ints/solution_2.py
@@ -6,21 +6,12 @@ Greedy approach
 '''
 
 def pushDownValues(items, fromIndex):
-    for i in range(len(items) - 1, fromIndex + 1, -1):
+    # loop in reverse starting from end of array till the 'fromIndex' where the 'push down' should start
+    for i in range(len(items) - 1, fromIndex, -1):
         items[i] = items[i-1]
 
-int_list = [15, 10, 7, 8, 5, 11,  12, 9]
-
-max_list = int_list[:3]
-max_list.sort(reverse = True)
-
-print(f"Initial values: {max_list[0]}, {max_list[1]}, {max_list[2]}")
-print("-------------------------------------")
-
-for i in range(len(int_list) - 1):
-    current_value = int_list[i]
-    
-    print(f"Begin iteration values: {max_list[0]}, {max_list[1]}, {max_list[2]}; {current_value}")
+def sort_max(current_value):
+    print(f"Begin iteration values (max): {max_list[0]}, {max_list[1]}, {max_list[2]}; {current_value}")
 
     if (current_value >= max_list[0]):
         pushDownValues(max_list, 0)
@@ -31,8 +22,61 @@ for i in range(len(int_list) - 1):
     elif (current_value >= max_list[2]):
         max_list[2] = current_value
 
-    print(f"End iteration values: {max_list[0]}, {max_list[1]}, {max_list[2]}; {current_value}")
+    print(f"End iteration values (max): {max_list[0]}, {max_list[1]}, {max_list[2]}; {current_value}")
     print("-------------------------------------")
 
-print(f"Final values: {max_list[0]}, {max_list[1]}, {max_list[2]}")
+def sort_min(current_value):
+    print(f"Begin iteration values (min): {min_list[0]}, {min_list[1]}, {min_list[2]}; {current_value}")
+
+    if (current_value <= min_list[0]):
+        pushDownValues(min_list, 0)
+        min_list[0] = current_value
+    elif (current_value <= min_list[1]):
+        pushDownValues(min_list, 1)
+        min_list[1] = current_value
+    elif (current_value <= min_list[2]):
+        min_list[2] = current_value
+
+    print(f"End iteration values (min): {min_list[0]}, {min_list[1]}, {min_list[2]}; {current_value}")
+    print("-------------------------------------")
+
+def find_max_prod(min_list, max_list):
+    ''' 
+    Logic - take products of:
+        a) All items in max_list
+        b) First 2 items in min_list & 1st item in max_list -> if we have two large -ve numbers
+        c) return the max of a & b
+    '''
+    positive_max = max_list[0] * max_list[1] * max_list[2]
+    negative_max = min_list[0] * min_list[1] * max_list[0]
+
+    return max(positive_max, negative_max)
+
+
+if __name__ == "__main__":
+    int_list = [15, 10, 7, 8, 5, 11, 12, 9, 6, -12, -13]
+    # int_list = [1, 2, 5, 6, 7, 8, 9, 10, 4, 77]
+
+    max_list = int_list[:3]
+    max_list.sort(reverse = True)
+
+    min_list = int_list[:3]
+    min_list.sort()
+
+    print(f"Initial max values: {max_list[0]}, {max_list[1]}, {max_list[2]}")
+    print(f"Initial min values: {min_list[0]}, {min_list[1]}, {min_list[2]}")
+    print("-------------------------------------")
+
+    for i in range(3, len(int_list)):
+        current_value = int_list[i]
+        
+        sort_max(current_value)
+        sort_min(current_value)
+
+        max_product = find_max_prod(min_list, max_list)
+
+
+    print(f"Final values: {max_list[0]}, {max_list[1]}, {max_list[2]}")
+    print(f"Final values: {min_list[0]}, {min_list[1]}, {min_list[2]}")
+    print(f"Max product: {max_product}")
 
